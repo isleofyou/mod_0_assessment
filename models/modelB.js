1b4bf7a02dfb2c5d44a65eacc646610e3da12b81
David Tran <frozenpho@gmail.com>
2021-07-30 12:54:49 -0600

    Add house information

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var HOUSE = {
+  neighborhood: "Prairie Walk",
+  city: "Parker",
+  floorNumber: 3,
+  garageSpaces: 1,
+  hasPool: true,
+  hasGym: true,
+  features: ["Fireplace", "Vaulted Ceilings", "Balcony"],
+  rooms: ["Bedroom", "Bathroom", "Kitchen"],
+}
