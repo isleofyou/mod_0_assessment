91b163cf85ae781b2d56f8a2b4c3185f6d990389
David Tran <frozenpho@gmail.com>
2021-07-30 12:50:05 -0600

    Add laptop information

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var LAPTOP = {
+  make: "Mac",
+  chip: "M1",
+  screenSize: 13,
+  memory: 256,
+  hasHeadphonePort: false,
+  hasStickers: true,
+  buttons: ["Escape", "Shift", "Return"],
+  programs: ["Safari", "Photobooth", "Calendar"],
+}
