389fe63c8b706241d8d12a0b1d432a791ac9bd33
David Tran <frozenpho@gmail.com>
2021-07-30 12:45:40 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = "Koala";
+var isCuddly = true;
+animal = "Panda";
+var num8 = 8;
+var num3 = 3;
+var quotient = (num3 / num8);
+var favoriteFilms = ["Pineapple Express", "The Conjuring", "Step Brothers"];
+favoriteFilms.pop();
+console.log(animal.length);
